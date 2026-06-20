e9c400df00702d8258ab6227f0960cededdb7e5a
joseycode <jljackovitch1@malone.edu>
2026-06-20 01:05:24 -0400

    Feat: Add Base64 playlist cover syncing, fullscreen mobile now playing card, Media Session lockscreen controls, flat bottom bar, and launcher icon placeholder

--- app/src/main/java/com/example/retroclone/MainActivity.java
+++ app/src/main/java/com/example/retroclone/MainActivity.java
@@ -710,12 +710,23 @@ public class MainActivity extends AppCompatActivity implements AudioService.Serv
             Bitmap decodedBitmap = null;
             try {
                 if (isUri) {
-                    InputStream inputStream = getContentResolver().openInputStream(Uri.parse(artworkPath));
-                    BitmapFactory.Options decodeOptions = new BitmapFactory.Options();
-                    decodeOptions.inSampleSize = qualityMode;
-                    decodedBitmap = BitmapFactory.decodeStream(inputStream, null, decodeOptions);
-                    if (inputStream != null) {
-                        inputStream.close();
+                    if (artworkPath.startsWith("data:image/")) {
+                        int commaIndex = artworkPath.indexOf(",");
+                        if (commaIndex != -1) {
+                            String base64Data = artworkPath.substring(commaIndex + 1);
+                            byte[] decodedBytes = Base64.decode(base64Data, Base64.DEFAULT);
+                            BitmapFactory.Options decodeOptions = new BitmapFactory.Options();
+                            decodeOptions.inSampleSize = qualityMode;
+                            decodedBitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length, decodeOptions);
+                        }
+                    } else {
+                        InputStream inputStream = getContentResolver().openInputStream(Uri.parse(artworkPath));
+                        BitmapFactory.Options decodeOptions = new BitmapFactory.Options();
+                        decodeOptions.inSampleSize = qualityMode;
+                        decodedBitmap = BitmapFactory.decodeStream(inputStream, null, decodeOptions);
+                        if (inputStream != null) {
+                            inputStream.close();
+                        }
                     }
                 } else {
                     MediaMetadataRetriever retriever = null;
@@ -1221,9 +1232,16 @@ public class MainActivity extends AppCompatActivity implements AudioService.Serv
                 try {
                     getContentResolver().takePersistableUriPermission(documentUri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
                 } catch (Exception ignored) {}
-                activePlaylistForImage.imageUri = documentUri.toString();
-                savePlaylists();
-                filterData("");
+                new Thread(() -> {
+                    String b64 = getBase64Image(documentUri);
+                    if (!b64.isEmpty()) {
+                        activePlaylistForImage.imageUri = "data:image/jpeg;base64," + b64.replaceAll("\\s", "");
+                        runOnUiThread(() -> {
+                            savePlaylists();
+                            filterData("");
+                        });
+                    }
+                }).start();
             }
         });
 
