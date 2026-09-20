5eb96feae1580151c9bba027e1f608be3706aa25
Josey Jackovitch <josey.jackovitch@lsicorp.com>
2026-09-19 22:13:03 -0400

    chore: bump version to 3.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.example.retroclone"
         minSdk = 24
         targetSdk = 36
-        versionCode = 18
-        versionName = "3.0.1"
+        versionCode = 19
+        versionName = "3.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
